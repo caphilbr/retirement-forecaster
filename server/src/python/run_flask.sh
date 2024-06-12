23c701f335c8b8064f9d1b763f3369dc088941aa
caphilbr <caphilbr@hotmail.com>
2024-06-12 08:34:09 -0400

    concurrent python and express servers

---
+++ server/src/python/run_flask.sh
@@ -0,0 +1,8 @@
+# server/src/python/run_flask.sh
+#!/bin/bash
+
+# Activate the virtual environment
+source server/src/python/venv/bin/activate
+
+# Run the Flask script
+python server/src/python/flask_api.py
